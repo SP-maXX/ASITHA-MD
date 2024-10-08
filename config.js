65f509464d0d1359518f5df535080a57fe80f1ee
SP-maXX <maxspeeder12@gmail.com>
2024-10-08 19:58:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "9qFB1D7Z#OqL_XARayh9-VGFyF-1iq6-OXRrNhA1p3k3c1utpaVY",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
